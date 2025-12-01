7097f0a30c6f9c2d62f37853c048f633f373f057
fixed27 <gilbertodamian14@gmail.com>
2025-11-30 20:58:08 -0600

    Arreglar tamaño de imagen principal

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
         <p className="subtitle">Apasionado por construir productos digitales con un diseño simple, funcional y responsivo.</p>
 
         <div className="about-grid">
-          <img src="/img/canales.jpg" alt="Foto de Sergio Canales" width="420" height="685" />
+          <img src="/img/canales.jpg" alt="Foto de Sergio Canales" />
 
           <div>
             <p>Soy un desarrollador Full Stack centrado en la creación de interfaces limpias y APIs eficientes. Me encanta resolver problemas reales usando tecnologías modernas, siguiendo buenas prácticas y manteniendo un enfoque minimalista en el diseño.</p>
